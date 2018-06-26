c2ae8756a01ea7c6f38b715dcd78094993c7b126
grace <gracewebb20619@gmail.com>
2018-06-26 11:37:59 -0400

    second

---
+++ main.js
@@ -0,0 +1,19 @@
+//Display an array from the cities in gotCitiesCSV
+
+//Display an array of words from the sentence in bestThing
+
+//Display a string separated by semi-colons instead of commas from gotCitiesCSV
+
+//Display a CSV (comma-separated) string from the lotrCitiesArray
+
+//Display the last 5 cities in lotrCitiesArray
+
+//Using splice, remove "Rohan" from lotrCitiesArra
+
+//Using slice, display characters between the 23rd and 38th position of bestThing (i.e., "boolean is even"
+
+//Find and display the index of "only" in bestThing
+
+//Find and display all cities from gotCitiesCSV  that use double vowels ("aa","ee", etc.)
+
+//Sort lotrCitiesArray by the number of characters in each city (i.e., shortest city names go first)
