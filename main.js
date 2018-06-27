57c832c3b48058da9adc2cbd4a2b2d1f579a0ee0
grace <gracewebb20619@gmail.com>
2018-06-27 13:00:55 -0400

    finally

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
+const gotCitiesCSV = "King's Landing,Braavos,Volantis,Old Valyria,Free Cities,Qarth,Meereen";
+const lotrCitiesArray = ["Mordor","Gondor","Rohan","Beleriand","Mirkwood","Dead Marshes","Rhun","Harad"];
+const bestThing = "The best thing about a boolean is even if you are wrong you are only off by a bit"
 //Display an array from the cities in gotCitiesCSV
-var cities = ['kings landing', 'bravos', 'volantis', 'Old Valyria', 'free cities', 'Qarth', 'Meereen']
-cities;
+k1 = gotCitiesCSV.split(',');
+console.log(k1);
 //Display an array of words from the sentence in bestThing
-var bestThing = ["The best thing about a boolean is even if you are wrong you are only off by a bit"]
-bestThing;
+var k2 = bestThing.split(" ");
+console.log(k2);
+
 //Display a string separated by semi-colons instead of commas from gotCitiesCSV
-
+var k3 = k1.join(";");
+console.log(k3);
 //Display a CSV (comma-separated) string from the lotrCitiesArray
-
+var k4 = lotrCitiesArray.join(",");
+console.log(k4);
 //Display the last 5 cities in lotrCitiesArray
-
+var k5= [];
+for (var i= 3; i < 8; i++) {
+    var arrayItem= lotrCitiesArray[i];
+    k5.push(arrayItem);
+}
+console.log(k5);
 //Using splice, remove "Rohan" from lotrCitiesArra
 
 //Using slice, display characters between the 23rd and 38th position of bestThing (i.e., "boolean is even"
